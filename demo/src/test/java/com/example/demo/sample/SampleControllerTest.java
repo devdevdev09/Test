fb43998868e43e6fdc9a1ca687b44e2fb23c58fe
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-08-22 00:41:10 +0900

    test idonno

--- demo/src/test/java/com/example/demo/sample/SampleControllerTest.java
+++ demo/src/test/java/com/example/demo/sample/SampleControllerTest.java
@@ -6,17 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.example.demo.ApidocumetationTest;
 
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleControllerTest.class)
 public class SampleControllerTest extends ApidocumetationTest {
 
     @Autowired
