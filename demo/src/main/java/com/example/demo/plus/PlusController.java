41f5acf14552a6569df4488aa51af04992baae6a
dev09 <devdevdevdevdev09@gmail.com>
2020-08-25 15:33:39 +0900

    add sample api

--- demo/src/main/java/com/example/demo/plus/PlusController.java
+++ demo/src/main/java/com/example/demo/plus/PlusController.java
@@ -1,5 +1,10 @@
 package com.example.demo.plus;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlusController {
     
     @GetMapping("/return/reqReturnReceipt.json")
-    public String returnReceipt(String status){
-        return "Success";
+    public Map<String, Object> returnReceipt(String copOrdNo,
+                                            String copOrdGoodSeqs){
+        
+        System.out.println("copOrdNo : " + copOrdNo);
+        System.out.println("copOrdGoodSeqs : " + copOrdGoodSeqs);
+
+        List<String> ordGoodSeqList =  Arrays.asList(copOrdGoodSeqs.split(","));
+
+        Map<String, Object> result = new HashMap<String, Object>();
+
+        Map<String, String> resultData = new HashMap<String, String>();
+
+        for(String ordGoodSeq : ordGoodSeqList){
+            resultData.put(ordGoodSeq, "Error");
+        }
+
+        result.put("message", null);
+        result.put("result", resultData);
+        result.put("code", 200);
+        result.put("status", null);
+        result.put("requestId", "efecde50-6050-46f3-bcce-ad414f7c465e768473411");
+        result.put("etc", null);
+        
+        return result;
     }
 }
--- demo/src/test/java/com/example/demo/sample/SampleControllerTest.java
+++ demo/src/test/java/com/example/demo/sample/SampleControllerTest.java
@@ -6,26 +6,47 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
     @Autowired
     TestRestTemplate testRestTemplate;
 
-    @Test
-    public void sample1() {
-        // String result = testRestTemplate.getForObject("/sample1", String.class);
-        ResponseEntity<String> response = testRestTemplate.getForEntity("/sample1" + "?test=test", String.class);
+    @Autowired
+    private MockMvc mockMvc;
 
-        assertEquals(response.getBody(), "test");
-        assertEquals(response.getStatusCode(), HttpStatus.OK);
-        assertNotEquals(response.getBody(), "test222");
+    @Test
+    public void sample1() throws Exception {
+        // Test1.
+        // String result = testRestTemplate.getForObject("/sample1", String.class);
+
+        // Test2.
+        // ResponseEntity<String> response = testRestTemplate.getForEntity("/sample1" + "?test=test", String.class);
+
+        // assertEquals(response.getBody(), "test");
+        // assertEquals(response.getStatusCode(), HttpStatus.OK);
+        // assertNotEquals(response.getBody(), "test222");
+
+        // Test3.
+        MultiValueMap<String, String> info = new LinkedMultiValueMap();
+
+        info.add("test", "heo");
+
+        this.mockMvc.perform(get("/sample1")
+            .params(info)
+            .andExpect(status().isOk())
+            .andExpect(content().string("heo"))
+            .andDo(print());
     }
 }
